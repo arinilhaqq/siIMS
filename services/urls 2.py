2bb41d3fd151aa7a1cd71330e9a5dbe09fe7c055
Sophie Andira Cindani Gardian <sophie.andira@ui.ac.id>
2023-03-17 02:29:01 +0700

    udah dong

--- services/urls 2.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import services_list, add_service
-
-urlpatterns = [
-    path('', services_list, name='services'),
-    path('addservice/', add_service, name='add'),
-]

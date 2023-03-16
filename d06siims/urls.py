47476627e33ba7c8dbc767cbc116922addb7c5f6
Sophie Andira Cindani Gardian <sophie.andira@ui.ac.id>
2023-03-16 23:49:06 +0700

    huft

--- d06siims/urls.py
+++ d06siims/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
-import services.urls as services
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('services/', include(services)),
+    path('', include('home.urls')),
+    path('', include('pelanggan.urls')),
+    path('', include('karyawan.urls')),
+    path('', include('services.urls')),
 ]
